5bfc7508ff2c50b4faf3deb0527627cc6293e649
Irfani Silviana <ir.silviana@gmail.com>
2024-01-26 19:37:07 +0100

    Implement Mobile menu - NavBar component

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { SignedIn, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import Theme from './Theme';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
@@ -33,6 +34,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
+        <MobileNav />
       </div>
     </nav>
   );

aede58ddb9c1ecc748abe769b6d8c6d8df6c24a4
Irfani Silviana <ir.silviana@gmail.com>
2024-01-27 17:44:16 +0100

    Add RightSidebar component

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,75 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const RightSidebar = () => {
+  const hotQuestions = [
+    {
+      _id: '1',
+      title:
+        'Would it be appropriate to point out an error in another paper during a referee report?',
+    },
+    { _id: '2', title: 'How can an airconditioning machine exist?' },
+    {
+      _id: '3',
+      title: 'What is an example of 3 numbers that do not make up a vector?',
+    },
+    {
+      _id: 4,
+      title:
+        'Why we are being interrogated every time crossing UK Border as citizen?',
+    },
+  ];
+
+  const popularTags = [
+    { _id: 1, name: 'JavaScript', count: 20152 },
+    { _id: 2, name: 'Next.JS', count: 85464 },
+    { _id: 3, name: 'React.JS', count: 85464 },
+    { _id: 4, name: 'TypeScript', count: 85464 },
+  ];
+
+  return (
+    <section className=" light-border background-light900_dark200 custom-scrollbar sticky right-0  top-0 flex  flex-col gap-16 border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[360px]">
+      <div className="flex flex-col gap-6">
+        <h3 className=" h3-bold text-dark200_light900 ">Top Questions</h3>
+        <div>
+          {hotQuestions.map((question) => (
+            <Link
+              href={`/questions/${question._id}`}
+              key={question._id}
+              className="text-dark200_light900 mb-7 flex cursor-pointer flex-row items-start justify-between"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron-right"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col gap-6">
+        <h3 className=" h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <Link
+              key={tag._id}
+              href={`/tags/${tag._id}`}
+              className="text-dark200_light900 flex-between  w-auto cursor-pointer"
+            >
+              <p className="background-light800_dark300 rounded px-4 py-2 text-light-400 ">
+                {tag.name}
+              </p>
+              <p className="small-medium text-dark500_light700">{tag.count}</p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default RightSidebar;

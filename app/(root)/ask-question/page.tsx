b26d2ac8b63e2c5767be06a1b946c42f2ce61c23
Irfani Silviana <ir.silviana@gmail.com>
2024-02-11 21:54:04 +0100

    Refactor user ID retrieval in ask-question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -4,9 +4,7 @@ import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
 const Page = async () => {
-  // const { userId } = auth();
-
-  const userId = '123456';
+  const { userId } = auth();
 
   if (!userId) {
     redirect('/sign-in');

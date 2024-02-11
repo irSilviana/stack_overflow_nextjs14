9a084c22d93faeda592cfc16077a8de0dd966cfe
Irfani Silviana <ir.silviana@gmail.com>
2024-02-11 22:24:25 +0100

    Update authMiddleware routes in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,20 @@
 import { authMiddleware } from '@clerk/nextjs';
 
-export default authMiddleware({ publicRoutes: ['/'] });
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    'api/webhooks',
+    'questions',
+    'questions/:id',
+    '/tags',
+    '/tags/:id',
+    '/questions/',
+    '/profile/:id',
+    '/community',
+    '/jobs',
+  ],
+  ignoredRoutes: ['/api/webhooks', '/api/chatgpt'],
+});
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],

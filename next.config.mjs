88d690721c9b072f4334b1052dee6e437add24b2
Irfani Silviana <ir.silviana@gmail.com>
2024-02-13 11:11:28 +0100

    Add remotePatterns to images configuration

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ const nextConfig = {
     mdxRs: true,
     serverComponentsExternalPackages: ['mongoose'],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '*',
+      },
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
+  },
 };
 
 export default nextConfig;

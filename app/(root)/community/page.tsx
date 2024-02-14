45c73e63b5fb20d1d94f462132ce5922aa54a3bb
Irfani Silviana <ir.silviana@gmail.com>
2024-02-14 21:24:48 +0100

    Refactor community page layout

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -10,13 +10,11 @@ export default async function Community() {
 
   return (
     <>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900"> All Users</h1>
-      </div>
+      <h1 className="h1-bold text-dark100_light900"> All Users</h1>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchBar
-          route="/"
+          route="/community"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
           placeholder="Search amazing minds here..."
@@ -24,12 +22,11 @@ export default async function Community() {
         />
         <Filter
           filters={UserFilters}
-          otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses="max-md:flex"
+          otherClasses="min-h-[56px] sm:min-w-[170px] "
         />
       </div>
 
-      <div className="mt-10 flex w-full flex-row gap-6">
+      <section className="mt-12 flex flex-wrap  gap-4">
         {result.users.length > 0 ? (
           result.users.map((user) => (
             <UserCard
@@ -53,7 +50,7 @@ export default async function Community() {
             linkTitle="Sign Up"
           />
         )}
-      </div>
+      </section>
     </>
   );
 }

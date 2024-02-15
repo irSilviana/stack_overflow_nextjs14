f6f11795ab07c4cd47e8f20c8bb06b389671df39
Irfani Silviana <ir.silviana@gmail.com>
2024-02-15 21:56:58 +0100

    Add getTopInteractedTags function to tag.action.ts

---
+++ lib/actions/tag.action.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import User from '@/database/user.model';
+import { connectToDatabase } from '../mongoose';
+import { GetTopInteractedTagsParams } from './shared.types';
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // find interactions for the user and group by tag
+    //  interaction
+
+    return [
+      { _id: '1', name: 'tag1', count: 1 },
+      { _id: '2', name: 'tag2', count: 2 },
+      { _id: '3', name: 'tag3', count: 3 },
+    ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}

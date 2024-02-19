ec09c025656b475c8aa2385fab4fb3a925e57327
Irfani Silviana <ir.silviana@gmail.com>
2024-02-19 11:55:06 +0100

    Refactor UserCard component structure

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -18,31 +18,30 @@ const UserCard = async ({ user }: UserCardProps) => {
   const interactedTags = await getTopInteractedTags({ userId: user._id });
 
   return (
-    <Link
-      href={`/profile/${user.username}`}
-      className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
-    >
+    <div className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]">
       <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
-        <Image
-          src={user.picture}
-          alt={`${user.name} profile picture`}
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
+        <Link href={`/profile/${user.username}`} className="">
+          <Image
+            src={user.picture}
+            alt={`${user.name} profile picture`}
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
 
-        <div className="mt-4 text-center">
-          <h3 className="h3-bold text-dark200_light900 line-clamp-1">
-            {user.name}
-          </h3>
-          <p className="body-regular text-dark500_light500 mt-2">
-            @{user.username}
-          </p>
-        </div>
+          <div className="mt-4 text-center">
+            <h3 className="h3-bold text-dark200_light900 line-clamp-1">
+              {user.name}
+            </h3>
+            <p className="body-regular text-dark500_light500 mt-2">
+              @{user.username}
+            </p>
+          </div>
+        </Link>
 
         <div className="mt-5">
           {interactedTags.length > 0 ? (
-            <div className="flex items-center gap-2">
+            <div className="flex justify-between gap-2">
               {interactedTags.map((tag) => (
                 <RenderTag
                   key={tag._id}
@@ -58,7 +57,7 @@ const UserCard = async ({ user }: UserCardProps) => {
           )}
         </div>
       </article>
-    </Link>
+    </div>
   );
 };
 export default UserCard;

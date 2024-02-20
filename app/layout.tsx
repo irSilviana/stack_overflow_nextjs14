8be283390edf63f396a919b3ab39325f8ee000ca
Irfani Silviana <ir.silviana@gmail.com>
2024-02-20 13:15:14 +0100

    Add prism.css stylesheet to the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
+import '../styles/prism.css';
 import React from 'react';
 import { ThemeProvider } from '@/context/themeProvider';
 

d43a655931e2978a7e7031250f23556fabed6b03
Irfani Silviana <ir.silviana@gmail.com>
2024-02-21 15:29:48 +0100

    Refactor Answer component and add createAnswer functionality

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -16,11 +16,20 @@ import { useRef, useState } from 'react';
 import { useTheme } from '@/context/themeProvider';
 import { Button } from '../ui/button';
 import Image from 'next/image';
+import { createAnswer } from '@/lib/actions/answer.action';
+import { usePathname } from 'next/navigation';
 
-const Answer = () => {
-  const [isSubmitting, setIsSubmitting] = useState(false);
+interface Props {
+  question: string;
+  questionId: string;
+  authorId: string;
+}
+
+const Answer = ({ question, questionId, authorId }: Props) => {
+  const pathname = usePathname();
   const { mode } = useTheme();
   const editorRef = useRef(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   // Define the form
   const form = useForm<z.infer<typeof AnswerSchema>>({
@@ -31,7 +40,32 @@ const Answer = () => {
   });
 
   // Define a submit handler
-  const handleCreateAnswer = () => {};
+  const handleCreateAnswer = async (values: z.infer<typeof AnswerSchema>) => {
+    setIsSubmitting(true);
+
+    try {
+      await createAnswer({
+        content: values.answer,
+        author: JSON.parse(authorId),
+        question: JSON.parse(questionId),
+        path: pathname,
+      });
+
+      // Reset the form
+      form.reset();
+
+      // Clear the editor
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+
+        editor.setContent('');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <div>
@@ -115,7 +149,7 @@ const Answer = () => {
 
           <div className="flex justify-end">
             <Button
-              type="button"
+              type="submit"
               className="primary-gradient w-fit text-white"
               disabled={isSubmitting}
             >

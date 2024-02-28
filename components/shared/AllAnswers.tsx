d232e9136de7b01fe893ec41766a7b0b4f29a853
Irfani Silviana <ir.silviana@gmail.com>
2024-02-28 20:14:13 +0100

    Add Votes component to AllAnswers

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -5,6 +5,7 @@ import { getTimeStamp } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import ParseHTML from './ParseHTML';
+import Votes from './Votes';
 
 interface Props {
   questionId: string;
@@ -54,7 +55,9 @@ const AllAnswers = async ({
                     </p>
                   </div>
                 </Link>
-                <div className="flex justify-end">VOTING</div>
+                <div className="flex justify-end">
+                  <Votes />
+                </div>
               </div>
             </div>
             <ParseHTML data={answer.content} />

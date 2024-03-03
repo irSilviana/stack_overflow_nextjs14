2dae419b591060871318f63a74cd7d0f61d5502d
Irfani Silviana <ir.silviana@gmail.com>
2024-03-03 18:28:07 +0100

    Add formatAndDivideNumber function to utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -61,3 +61,7 @@ export function formatNumber(num: number): string {
     return num.toString();
   }
 }
+
+export function formatAndDivideNumber(num: number): string {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
